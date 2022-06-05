cd43fafbfed4b528d88910523a3cdb6db57d0d0e
johnd31415 <johnd31415@yahoo.com>
2022-06-05 19:41:17 -0400

    First projectile AI attempt
     also shitty envy projectile sprite

--- Content/Projectiles/EnvyProjectile.cs
+++ Content/Projectiles/EnvyProjectile.cs
@@ -16,17 +16,33 @@ namespace slingin.Content.Projectiles
             Projectile.penetrate = 1;
             Projectile.width = 2;
             Projectile.height = 20;
-            Projectile.aiStyle = 1;
+            Projectile.aiStyle = 0;
             Projectile.friendly = true;
             Projectile.DamageType = DamageClass.Ranged;
             Projectile.timeLeft = 600;
-            AIType = ProjectileID.WoodenArrowFriendly;
+            Projectile.direction = 0;
+            //AIType = ProjectileID.WoodenArrowFriendly;
         }
         public override void Kill(int timeLeft)
         {
 
             Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
             SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+        }
+        public override void AI()
+        {
+            //Projectile.Distance();
+            float velXMult = 0.95f;
+            Projectile.velocity.X *= velXMult;
+            float velYDec = 0.15f;
+            //Main.NewText("X:" + Projectile.velocity.X + " Y:" + Projectile.velocity.Y, 63, 255, 63);
+            if (Math.Abs(Projectile.velocity.X) < 5f)
+            {
+                Projectile.velocity.Y += velYDec;
+            }
+
+            //float velRotation = Projectile.velocity.ToRotation();
+            Projectile.rotation = 0;//velRotation + MathHelper.ToRadians(90f);
         }
     }
 }
